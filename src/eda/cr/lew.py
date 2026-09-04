d4b4583cfc9970280f3376e859a7d0e1755c488f
Alexei A. Smekalkine <ikle@ikle.ru>
2026-09-04 12:06:33 +0300

    cr-lew: fix temporary test changes for history

--- src/eda/cr/lew.py
+++ src/eda/cr/lew.py
@@ -13,22 +13,37 @@ from eda.cr.vcg import VCG
 
 eps = frozenset ()
 
-def get_weight (P, n, L, R, MD, LD):
+def get_weight (P, n, L, R, MD, LD, V):
 	def fn (LD):
+		return 10 / (MD - LD + 1)
 		G = [10, 5, 3, 2, 1]
 		w = MD - LD
 		return G[w] if w < len (G) else 0
 
-	return sum (map (fn, LD [L[n] : R[n]+1]), P)
+#	return sum (map (fn, LD [L[n] : R[n]+1]), P)
+	return (sum (map (fn, LD [L[n] : R[n]+1]), P) +
+#	        (3 * V.depth (n) + len (V.child[n])) * (R[n]+1 - L[n]) * 0.1)
+	        0.3 * V.depth (n) * (R[n]+1 - L[n]))
 
-def get_track (F, L, R, LD, size):
+def get_depths (V, F):
+	D = [0] * len (V.child)
+
+	for n in F:
+		D[n] = V.depth (n)
+
+	base = max (D) + 2
+	return list (map (lambda x: 1 + 10 / (base - x), D))
+
+def get_track (F, L, R, LD, size, V):
 	ZP, ZA, P, A, MD = [0] * (size + 1), [0] * (size + 1), 0, 0, max (LD)
+	H = get_depths (V, F)
 
 	for n in F:  ZA[L[n]] = n  # max one net in top can start in same x
 
 	for i in reversed (range (size)):
 		if (n := ZA[i]) > 0:
-			NP = get_weight (ZP[R[n]+1], n, L, R, MD, LD)
+#			NP = get_weight (ZP[R[n]+1], n, L, R, MD, LD, V)
+			NP = ZP[R[n]+1] + H[n] * (R[n]+1 - L[n])
 
 			if NP >= P:
 				P, A = NP, n
@@ -53,9 +68,11 @@ def route (U, D, LE = eps, RE = eps):
 		if not (F := V.top):
 			raise ValueError ("Cycle detected")
 
-		for n in get_track (F, L, R, W, len (U)):
+#		LD = V.density (W, L, R)
+
+		for n in get_track (F, L, R, W, len (U), V):
 			V.remove (n)
-			cd_reduce (W, L[n], R[n])
+#			cd_reduce (W, L[n], R[n])
 			T[n] = t
 
 		t = t + 1
